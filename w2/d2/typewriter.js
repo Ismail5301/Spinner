52c95efe81c80aa727aef941b4ad39965418ba91
Abdurahman Ismail Abdi <ismail530@gmail.com>
2021-09-21 18:53:31 +0000

    typewriter files

---
+++ w2/d2/typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+let counter = 0;
+
+for (const char of sentence) {
+ 
+  setTimeout(() => {
+    process.stdout.write(char);  
+  }, counter );
+  counter += 100;
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, counter);
+  
+
